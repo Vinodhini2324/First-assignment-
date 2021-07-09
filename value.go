0faa251f1bf2dac0c4b6692346c3c3650c2cfa82
Vinodhini2324 <86877127+Vinodhini2324@users.noreply.github.com>
2021-07-09 10:48:37 +0530

    value.go

---
+++ value.go
@@ -0,0 +1,14 @@
+# First-assignment-
+package main
+import "fmt"
+func main() {
+
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("6.0/3.0 =", 6.0/3.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
